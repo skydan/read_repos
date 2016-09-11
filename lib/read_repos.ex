68488df90b1116239678490a58cb6e288c9346bb
Evgeniy Skidan <eskidan@gmail.com>
2016-09-11 18:10:29 +0300

    Add weighted slaves selection

--- lib/read_repos.ex
+++ lib/read_repos.ex
@@ -1,20 +1,24 @@
 defmodule ReadRepos do
   defmacro __using__(opts) do
-    master = __CALLER__.module
-    read_repo = Module.split(__CALLER__.module) |> List.first |> Module.concat(ReadRepo)
+    master    = __CALLER__.module
+    read_repo = Module.split(__CALLER__.module)
+      |> List.first
+      |> Module.concat(ReadRepo)
+
     quote location: :keep do
-      @master unquote(master)
-      @otp_app unquote(opts)[:otp_app] || Module.get_attribute(@master, :otp_app)
-      @regexp unquote(opts)[:regexp] || Regex.compile!(".*#{unquote(read_repo)}[0-9]+", "i")
+      @master    unquote(master)
+      @otp_app   unquote(opts)[:otp_app]   || Module.get_attribute(@master, :otp_app)
+      @regexp    unquote(opts)[:regexp]    || Regex.compile!(".*#{unquote(read_repo)}[0-9]+", "i")
       @page_size unquote(opts)[:page_size] || 10
+
       Module.register_attribute __MODULE__, :slaves, accumulate: true
       @before_compile unquote(__MODULE__)
 
+      env      = Application.get_all_env(@otp_app)
       contents = quote location: :keep do
         use Ecto.Repo, otp_app: unquote(@otp_app)
       end
 
-      env = Application.get_all_env(@otp_app)
       Enum.each(env, fn({key, conf}) when is_atom(key) ->
         if Regex.match?(@regexp, Atom.to_string(key)) do
           Module.create(key, contents, Macro.Env.location(__ENV__))
@@ -28,14 +32,21 @@ defmodule ReadRepos do
     slaves = Module.get_attribute(env.module, :slaves) |> Enum.reverse
     master = Module.get_attribute(env.module, :master)
     slaves = [master | slaves]
-    count = length(slaves)
+    count  = length(slaves)
+
     quote location: :keep do
       def slaves do
         unquote(Macro.escape(slaves))
       end
+
       if unquote(count) > 1 do
         def slave do
-          at = Enum.random(0..unquote(Macro.escape(count)) - 1)
+          slaves_count  = unquote(Macro.escape(count))
+          master_weight = 2
+          slave_weight  = 3
+          total_weights = master_weight + slaves_count * slave_weight
+          rand = :rand.uniform(total_weights)
+          at   = if rand <= master_weight, do: 0, else: div(rand, slave_weight)
           Enum.fetch!(slaves, at)
         end
       else
